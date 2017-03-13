74c47582fcaa95dc4b05cb3648187d7b22736dd1
y-nemoto <y-nemoto@jp.fujitsu.com>
2017-03-13 10:31:20 +0900

    add port option

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/kaakaa/matterpoll-emoji/poll"
 )
 
+var port = flag.Int("p", 8080, "port number")
+
 func main() {
+	flag.Parse()
+
 	c, err := poll.LoadConf("config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 	poll.Conf = c
 	http.HandleFunc("/poll", poll.PollCmd)
-	http.ListenAndServe(":8081", nil)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
+		log.Fatal(err)
+	}
 }
