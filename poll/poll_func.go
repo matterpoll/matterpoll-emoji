47d4becbbaae05d440169d2310f7158a57280209
Ben Schumacher <hanzei@mailbox.org>
2017-07-24 15:09:31 +0200

    Post message as bot

--- poll/poll_func.go
+++ poll/poll_func.go
@@ -1,10 +1,11 @@
 package poll
 
 import (
-	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	//"time"
 
 	"github.com/mattermost/platform/model"
 )
@@ -15,29 +16,67 @@ func PollCmd(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Print(err)
+		log.Print("Error", err)
 		return
 	}
+
 	poll, err := NewPollRequest(string(b))
 	if err != nil {
-		log.Print(err)
+		log.Print("Error", err)
 		return
 	}
+
+	var responce = `{
+		"response_type": "in_channel",
+		"text": "` + poll.Message + ` #poll",
+		"username": "Poll Bot",
+		"icon_url": "https://www.mattermost.org/wp-content/uploads/2016/04/icon.png"
+	}`
+
 	c := model.NewClient(Conf.Host)
 	c.TeamId = poll.TeamId
 
-	_, err = login(c)
+	var user *model.User
+	user, err = login(c)
+	var userId = user.Id
 	if err != nil {
-		log.Print(err)
+		log.Print("Error", err)
 		return
 	}
-	p, err := post(c, poll)
+
+	var result *model.Result
+	io.WriteString(w, responce)
+	/*
+		var start = time.Now().Unix()
+
+		time.Sleep(30 * time.Second)
+
+		result, err = c.GetPostsSince(poll.ChannelId, start)
+		if err != nil {
+			log.Println("Error", err)
+			return
+		}
+	*/
+	result, err = c.GetPosts(poll.ChannelId, 0, 10, "")
 	if err != nil {
-		log.Print(err)
+		log.Println("Error", err)
 		return
 	}
-	reaction(c, p, poll)
-	fmt.Fprintf(w, "{'text': 'hello'}")
+
+	var postId = result.Data.(*model.PostList).Order[0]
+	log.Println(result.Data.(*model.PostList).Order)
+	for _, postId := range result.Data.(*model.PostList).Order {
+		log.Println("Message is:", result.Data.(*model.PostList).Posts[postId].Message)
+	}
+	log.Println("PostId is:", result.Data.(*model.PostList).Posts[postId].Id)
+	log.Println("Message is:", result.Data.(*model.PostList).Posts[postId].Message)
+	log.Println("UserId is:", result.Data.(*model.PostList).Posts[postId].UserId)
+
+	err = reaction(c, poll.ChannelId, userId, postId, poll.Emojis)
+	if err != nil {
+		log.Print("Error", err)
+		return
+	}
 }
 
 func login(c *model.Client) (*model.User, error) {
@@ -60,13 +99,17 @@ func post(c *model.Client, poll *PollRequest) (*model.Post, error) {
 	return r.Data.(*model.Post), nil
 }
 
-func reaction(c *model.Client, p *model.Post, poll *PollRequest) {
-	for _, e := range poll.Emojis {
+func reaction(c *model.Client, channelId string, userId string, postId string, emojis []string) error {
+	for _, e := range emojis {
 		r := model.Reaction{
-			UserId:    p.UserId,
-			PostId:    p.Id,
+			UserId:    userId,
+			PostId:    postId,
 			EmojiName: e,
 		}
-		c.SaveReaction(p.ChannelId, &r)
+		_, err := c.SaveReaction(channelId, &r)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
