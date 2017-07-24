719ba6bf5b5e1125c3823d06b9ada26185127758
Hanzei <Hanzei@users.noreply.github.com>
2017-07-24 13:31:39 +0200

    Add support for single and double quotation marks (#12)
    * Add support for " and '
    * Throw error if first and last quotes dont match
    * Fix as requested

--- poll/poll_request.go
+++ poll/poll_request.go
@@ -32,28 +32,45 @@ func NewPollRequest(s string) (*PollRequest, error) {
 				return nil, fmt.Errorf("Unexpected Error: ChannelID in request is empty.")
 			}
 		case "text":
-			re := regexp.MustCompile("`([^`]+)`(.+)")
-			e := re.FindStringSubmatch(values[0])
-			if len(e) != 3 {
-				return nil, fmt.Errorf("Command Error: /poll `Here is description` :thumbsup: :thumbsdown:...")
+			p.Message, p.Emojis, err = parseText(values[0])
+			if err != nil {
+				return nil, err
 			}
-			var emojis []string
-			for _, v := range strings.Split(e[2], " ") {
-				if len(v) == 0 {
-					continue
-				}
-				if len(v) < 3 || !strings.HasPrefix(v, ":") || !strings.HasSuffix(v, ":") {
-					return nil, fmt.Errorf("Emoji Error: %s is not emoji format", v)
-				}
-				emojis = append(emojis, v[1:len(v)-1])
-			}
-			v := strings.Split(values[0], " ")
-			if len(v) < 2 {
-				return nil, fmt.Errorf("Error: /poll description emoji1 emoji2...")
-			}
-			p.Message, p.Emojis = e[1], emojis
 		default:
 		}
 	}
 	return p, nil
+}
+
+func parseText(text string) (string, []string, error) {
+	var re *(regexp.Regexp)
+	switch text[0] {
+	case '`':
+		re = regexp.MustCompile("`([^`]+)`(.+)")
+	case '\'':
+		re = regexp.MustCompile("'([^']+)'(.+)")
+	case '"':
+		re = regexp.MustCompile("\"([^\"]+)\"(.+)")
+	default:
+		return "", nil, fmt.Errorf("Command Error: /poll `Here is description` :thumbsup: :thumbsdown:...")
+	}
+	e := re.FindStringSubmatch(text)
+	if len(e) != 3 {
+		return "", nil, fmt.Errorf("Command Error: /poll `Here is description` :thumbsup: :thumbsdown:...")
+	}
+	var emojis []string
+	for _, v := range strings.Split(e[2], " ") {
+		if len(v) == 0 {
+			continue
+		}
+		if len(v) < 3 || !strings.HasPrefix(v, ":") || !strings.HasSuffix(v, ":") {
+			return "", nil, fmt.Errorf("Emoji Error: %s is not emoji format", v)
+		}
+		emojis = append(emojis, v[1:len(v)-1])
+	}
+	v := strings.Split(text, " ")
+	if len(v) < 2 {
+		return "", nil, fmt.Errorf("Error: /poll description emoji1 emoji2...")
+	}
+	return e[1], emojis, nil
 }
