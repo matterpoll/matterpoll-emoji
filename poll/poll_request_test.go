3c14f2f9dd39a821febdbbfd10ac53daa7fcae27
Hanzei <Hanzei@users.noreply.github.com>
2017-07-30 09:35:38 +0200

    Add tests for token parsing (#31)
    * Add tests for token parsing
    
    * Fix tests
    
    * fix indent

--- poll/poll_request_test.go
+++ poll/poll_request_test.go
@@ -9,66 +9,70 @@ func TestNewPollRequest(t *testing.T) {
 	tables := []struct {
 		teamId    string
 		channelId string
+		token     string
 		text      string
 		message   string
 		emojis    []string
 		err       error
 	}{
-		{"teamidxxxx", "channelidxxxx", "`description` :emoji1:", "description", []string{"emoji1"}, nil},
-		{"teamidxxxx", "channelidxxxx", "'description' :emoji1:", "description", []string{"emoji1"}, nil},
-		{"teamidxxxx", "channelidxxxx", "\"description\" :emoji1:", "description", []string{"emoji1"}, nil},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "`description` :emoji1:", "description", []string{"emoji1"}, nil},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "'description' :emoji1:", "description", []string{"emoji1"}, nil},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "\"description\" :emoji1:", "description", []string{"emoji1"}, nil},
 
-		{"teamidxxxx", "channelidxxxx", "`description including space` :emoji1: :emoji2: :emoji3:", "description including space", []string{"emoji1", "emoji2", "emoji3"}, nil},
-		{"teamidxxxx", "channelidxxxx", "'description including space' :emoji1: :emoji2: :emoji3:", "description including space", []string{"emoji1", "emoji2", "emoji3"}, nil},
-		{"teamidxxxx", "channelidxxxx", "\"description including space\" :emoji1: :emoji2: :emoji3:", "description including space", []string{"emoji1", "emoji2", "emoji3"}, nil},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "`description including space` :emoji1: :emoji2: :emoji3:", "description including space", []string{"emoji1", "emoji2", "emoji3"}, nil},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "'description including space' :emoji1: :emoji2: :emoji3:", "description including space", []string{"emoji1", "emoji2", "emoji3"}, nil},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "\"description including space\" :emoji1: :emoji2: :emoji3:", "description including space", []string{"emoji1", "emoji2", "emoji3"}, nil},
 
-		{"teamidxxxx", "channelidxxxx", "`description including space` :emoji1:  :emoji2:   :emoji3:", "description including space", []string{"emoji1", "emoji2", "emoji3"}, nil},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "`description including space` :emoji1:  :emoji2:   :emoji3:", "description including space", []string{"emoji1", "emoji2", "emoji3"}, nil},
 
-		{"teamidxxxx", "channelidxxxx", "description including space` :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("Start quotation mark missing")},
-		{"teamidxxxx", "channelidxxxx", "`description including space :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("End quotation mark missing")},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "description including space` :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("Start quotation mark missing")},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "`description including space :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("End quotation mark missing")},
 
-		{"teamidxxxx", "channelidxxxx", "`description including space' :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("First and second quotes do not match")},
-		{"teamidxxxx", "channelidxxxx", "'description including space\" :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("First and second quotes do not match")},
-		{"teamidxxxx", "channelidxxxx", "\"description including space` :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("First and second quotes do not match")},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "`description including space' :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("First and second quotes do not match")},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "'description including space\" :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("First and second quotes do not match")},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "\"description including space` :emoji1: :emoji2: :emoji3:", "", []string{""}, fmt.Errorf("First and second quotes do not match")},
 
-		{"", "channelidxxxx", "`description` :emoji1:", "", []string{}, fmt.Errorf("Unexpected Error: TeamID in request is empty.")},
-		{"teamidxxxx", "", "`description` :emoji1:", "", []string{}, fmt.Errorf("Unexpected Error: ChannelID in request is empty.")},
+		{"", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "`description` :emoji1:", "", []string{}, fmt.Errorf("Unexpected Error: TeamID in request is empty.")},
+		{"teamidxxxx", "", "9jrxak1ykxrmnaed9cps9i4cim", "`description` :emoji1:", "", []string{}, fmt.Errorf("Unexpected Error: ChannelID in request is empty.")},
+    {"teamidxxxx", "channelidxxxx", "", "`description` :emoji1:", "", []string{}, fmt.Errorf("Unexpected Error: Token in request is empty.")},
 
-		{"teamidxxxx", "channelidxxxx", "`description including space`", "", []string{}, fmt.Errorf("No emoji found")},
-		{"teamidxxxx", "channelidxxxx", "`description including space` emoji1", "", []string{}, fmt.Errorf("Emoji format error")},
-		{"teamidxxxx", "channelidxxxx", "`` :emoji1:", "", []string{""}, fmt.Errorf("No description found")},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "`description including space`", "", []string{}, fmt.Errorf("No emoji found")},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "`description including space` emoji1", "", []string{}, fmt.Errorf("Emoji format error")},
+		{"teamidxxxx", "channelidxxxx", "9jrxak1ykxrmnaed9cps9i4cim", "`` :emoji1:", "", []string{""}, fmt.Errorf("No description found")},
 	}
 
-	for _, tt := range tables {
-		s := fmt.Sprintf("team_id=%s&channel_id=%s&text=%s", tt.teamId, tt.channelId, tt.text)
-
+	for i, tt := range tables {
+		s := fmt.Sprintf("team_id=%s&channel_id=%s&token=%s&text=%s", tt.teamId, tt.channelId, tt.token, tt.text)
 		p, err := NewPollRequest(s)
 
 		if err != nil && tt.err == nil {
-			t.Fatalf("Test retured with error : %v but there sould be none", err)
+			t.Fatalf("Test %v retured with error : %v but there sould be none", i, err)
 		}
 		if err == nil && tt.err != nil {
-			t.Fatalf("Test didnt not return with an error but it should return with %v", tt.err)
+			t.Fatalf("Test %v didnt not return with an error but it should return with %v", i, tt.err)
 		}
 		if err != nil && tt.err != nil {
 			continue
 		}
 
 		if p.TeamId != tt.teamId {
-			t.Errorf("Assertion error `TeamId`. Expected: %s, Actual: %s.", tt.teamId, p.TeamId)
+			t.Errorf("Test %v: Assertion error `TeamId` in Expected: %s, Actual: %s.", i, tt.teamId, p.TeamId)
 		}
 		if p.ChannelId != tt.channelId {
-			t.Errorf("Assertion error `ChannelId`. Expected: %s, Actual: %s.", tt.channelId, p.ChannelId)
+			t.Errorf("Test %v: Assertion error `ChannelId`. Expected: %s, Actual: %s.", i, tt.channelId, p.ChannelId)
+		}
+    if p.Token != tt.token {
+			t.Errorf("Test %v: Assertion error `Token`. Expected: %s, Actual: %s.", i, tt.token, p.Token)
 		}
 		if p.Message != tt.message {
-			t.Errorf("Assertion error `Message`. Expected: %s, Actual: %s.", tt.message, p.Message)
+			t.Errorf("Test %v: Assertion error `Message`. Expected: %s, Actual: %s.", i, tt.message, p.Message)
 		}
 		if len(p.Emojis) != len(tt.emojis) {
-			t.Errorf("Assertion error `Emojis`. Expected: %s, Actual: %s.", tt.emojis, p.Emojis)
+			t.Errorf("Test %v: Assertion error `Emojis`. Expected: %s, Actual: %s.", i, tt.emojis, p.Emojis)
 		}
 		for i, v := range p.Emojis {
 			if v != tt.emojis[i] {
-				t.Errorf("Assertion error `Emojis`. Expected: %s, Actual: %s.", tt.emojis, p.Emojis)
+				t.Errorf("Test %v: Assertion error `Emojis`. Expected: %s, Actual: %s.", i, tt.emojis, p.Emojis)
 			}
 		}
 	}
