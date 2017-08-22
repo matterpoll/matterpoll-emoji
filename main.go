75ae46adf4e5d948e457ee4423dab03eb396e818
Oliver Palmer <oliverpalmer@opalmer.com>
2017-08-22 12:10:21 -0400

    allow an optional address when binding
    By default matterpoll-emoji binds to all addresses
    which if you're not behind a firewall, or the firewall
    the firewall is misconfigured, may be insecure. This
    commit does not change the default to keep from breaking
    existing installations.

--- main.go
+++ main.go
@@ -9,19 +9,23 @@ import (
 	"github.com/kaakaa/matterpoll-emoji/poll"
 )
 
-var port = flag.Int("p", 8505, "port number")
+var (
+	port    = flag.Int("p", 8505, "port number")
+	address = flag.String("a", "", "optional address to bind and listen on")
+	config  = flag.String("c", "config.json", "optional path to the config file")
+)
 
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	flag.Parse()
 
-	c, err := poll.LoadConf("config.json")
+	c, err := poll.LoadConf(*config)
 	if err != nil {
 		log.Fatal(err)
 	}
 	poll.Conf = c
 	http.HandleFunc("/poll", poll.PollCmd)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", *address, *port), nil); err != nil {
 		log.Fatal(err)
 	}
 }
